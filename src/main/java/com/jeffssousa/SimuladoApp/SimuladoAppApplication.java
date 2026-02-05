908522f74488dd76319355c9e524ddd1694ee0c5
Jefferson Sousa <jejefferson57@gmail.com>
2026-02-05 16:57:32 -0300

    feat: create project

---
+++ src/main/java/com/jeffssousa/SimuladoApp/SimuladoAppApplication.java
@@ -0,0 +1,13 @@
+package com.jeffssousa.SimuladoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuladoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeffssousa/SimuladoApp/SimuladoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffssousa.SimuladoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
