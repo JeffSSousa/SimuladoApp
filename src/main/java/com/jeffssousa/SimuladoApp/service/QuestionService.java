0e419ce933c46bcd294bc3bc13b700cf347772c5
Jefferson Sousa <jejefferson57@gmail.com>
2026-02-09 22:58:19 -0300

    feat: create findById in QuestionService

--- src/main/java/com/jeffssousa/SimuladoApp/service/QuestionService.java
+++ src/main/java/com/jeffssousa/SimuladoApp/service/QuestionService.java
@@ -2,8 +2,11 @@ package com.jeffssousa.SimuladoApp.service;
 
 import com.jeffssousa.SimuladoApp.entities.Question;
 import com.jeffssousa.SimuladoApp.repository.QuestionRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -13,5 +16,11 @@ public class QuestionService {
 
     public Question save(Question question) {
         return repository.save(question);
+    }
+
+    public Question findById(UUID questionId) {
+
+        return repository.findById(questionId)
+                                        .orElseThrow(() -> new EntityNotFoundException("Questão não encontrada!"));
     }
 }
--- src/test/java/com/jeffssousa/SimuladoApp/services/QuestionServiceTest.java
+++ src/test/java/com/jeffssousa/SimuladoApp/services/QuestionServiceTest.java
@@ -4,6 +4,7 @@ import com.jeffssousa.SimuladoApp.builders.QuestionTestBuilder;
 import com.jeffssousa.SimuladoApp.entities.Question;
 import com.jeffssousa.SimuladoApp.repository.QuestionRepository;
 import com.jeffssousa.SimuladoApp.service.QuestionService;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -12,7 +13,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -44,6 +48,46 @@ public class QuestionServiceTest {
             assertEquals(savedQuestion.getDescription(), question.getDescription());
             assertEquals(savedQuestion.getCategory(), question.getCategory());
 
+        }
+
+    }
+
+    @Nested
+    class findById{
+
+        @Test
+        @DisplayName("Deve Buscar Questão pelo id com sucesso")
+        void shouldFindByIdQuestionWithSuccess(){
+
+            Question question = QuestionTestBuilder.aQuestion()
+                                                    .withQuestionId(UUID.randomUUID())
+                                                    .build();
+
+            when(repository.findById(any(UUID.class))).thenReturn(Optional.of(question));
+
+            Question returnedQuestion = service.findById(question.getQuestionId());
+
+            verify(repository, times(1)).findById(any(UUID.class));
+
+            assertNotNull(returnedQuestion);
+            assertEquals(question.getDescription(),returnedQuestion.getDescription());
+            assertEquals(question.getCategory(),returnedQuestion.getCategory());
+
+        }
+
+        @Test
+        @DisplayName("Deve retornar uma exceção caso a Questão não exista")
+        void shouldThrowExceptionWhenQuestionNotFound(){
+
+            when(repository.findById(any(UUID.class))).thenReturn(Optional.empty());
+
+            EntityNotFoundException e = assertThrows(
+                    EntityNotFoundException.class,
+                    () -> service.findById(UUID.randomUUID())
+            );
+
+            verify(repository, times(1)).findById(any(UUID.class));
+            assertEquals("Questão não encontrada!", e.getMessage());
         }
 
     }
