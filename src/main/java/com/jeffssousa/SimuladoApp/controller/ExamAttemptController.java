6858828756f4d4e9412be6938b66db1473c01b23
Jefferson Sousa <jejefferson57@gmail.com>
2026-02-25 11:09:19 -0300

    feat: create answerQuestion method endpoint

--- src/main/java/com/jeffssousa/SimuladoApp/controller/ExamAttemptController.java
+++ src/main/java/com/jeffssousa/SimuladoApp/controller/ExamAttemptController.java
@@ -1,9 +1,11 @@
 package com.jeffssousa.SimuladoApp.controller;
 
+import com.jeffssousa.SimuladoApp.dto.AnswerQuestionDTO;
 import com.jeffssousa.SimuladoApp.dto.QuestionAlternativeResponseDTO;
 import com.jeffssousa.SimuladoApp.service.ExamAttemptService;
 import com.jeffssousa.SimuladoApp.usecase.session.StartAttemptUseCase;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,6 +30,12 @@ public class ExamAttemptController {
     public ResponseEntity<QuestionAlternativeResponseDTO> getCurrentQuestion(@PathVariable UUID id){
         QuestionAlternativeResponseDTO response = service.getCurrentQuestion(id);
         return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping("/answer-question")
+    public ResponseEntity<String> answerQuestion(@RequestBody AnswerQuestionDTO dto){
+        service.answerQuestion(dto);
+        return ResponseEntity.ok().body("Questão respondida com sucesso");
     }
 
 }
